e1461255fb72a93255f9649822516704a607bb0b
Ankur Agarwal <ankur.a.agarwal@gmail.com>
2019-08-20 21:39:51 -0700

    Add a lambda that runs a loop for 20 mins
    It will terminate before that b/c the maximum allowable value of timeout is 900 secs.
    Default is 6 secs.

--- handler.py
+++ handler.py
@@ -1,6 +1,8 @@
+import boto3
 import jinja2
 import json
 import logging
+import time
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -27,4 +29,9 @@ def lambda1(event, context):
     """
 
 def lambda2(event, context):
-    pass
+    # 20 mins = 1200 seconds. This lambda function will terminate with a timeout. See serverless.yml
+    # Default timeout is 6 seconds. And on setting a value > 900sec you will get an error during deployment:
+    #An error occurred: MyDashsimpleDashlambdaDashfunctionLambdaFunction - 1 validation error detected: Value '1200' at 'timeout' failed to satisfy constraint: Member must have value less than or equal to 900 (Service: AWSLambdaInternal; Status Code: 400; Error Code: ValidationException; Request ID: b2debad7-3ee6-432f-a3ce-c18592332371).
+    for i in range(1200):
+        logger.info("Iteration {}".format(i))
+        time.sleep(1)
