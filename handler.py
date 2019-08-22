f3fa6ce312c99d61ebc16042434daede212f211b
Ankur Agarwal <ankur.a.agarwal@gmail.com>
2019-08-21 20:41:55 -0700

    Add lambda that is triggered via a schedule
    schedule passes in a key value pair which is reported as the event
    
    Make sure your schedule yml constructs are correctly indented, otherwise you will not get an error but your event will not be created in the serverless stack and function won't be triggered.

--- handler.py
+++ handler.py
@@ -32,6 +32,8 @@ def lambda2(event, context):
     # 20 mins = 1200 seconds. This lambda function will terminate with a timeout. See serverless.yml
     # Default timeout is 6 seconds. And on setting a value > 900sec you will get an error during deployment:
     #An error occurred: MyDashsimpleDashlambdaDashfunctionLambdaFunction - 1 validation error detected: Value '1200' at 'timeout' failed to satisfy constraint: Member must have value less than or equal to 900 (Service: AWSLambdaInternal; Status Code: 400; Error Code: ValidationException; Request ID: b2debad7-3ee6-432f-a3ce-c18592332371).
+    logger.info("Received event is {}".format(event)) # {key1: value1, key2: value2 } is passed in as event 
+    logger.info("Received context is {}".format(context))
     for i in range(1200):
         logger.info("Iteration {}".format(i))
         time.sleep(1)
