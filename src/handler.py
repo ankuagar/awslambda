0851883155e9974893b908ac5c749c7710fcf599
Ankur Agarwal <ankur.a.agarwal@gmail.com>
2019-09-28 16:51:22 -0700

    Add service wide environment variable

--- src/handler.py
+++ src/handler.py
@@ -3,6 +3,7 @@ import jinja2
 import json
 import logging
 import time
+import os
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -16,6 +17,7 @@ def lambda1(event, context):
         "statusCode": 200,
         "body": json.dumps(body)
     }
+    logger.info("Service wide environment variable is {}".format(os.getenv('service_wide_env_variable')))
     logger.info("Response is {}".format(response))
     return response
 
@@ -32,7 +34,8 @@ def lambda2(event, context):
     # 20 mins = 1200 seconds. This lambda function will terminate with a timeout. See serverless.yml
     # Default timeout is 6 seconds. And on setting a value > 900sec you will get an error during deployment:
     #An error occurred: MyDashsimpleDashlambdaDashfunctionLambdaFunction - 1 validation error detected: Value '1200' at 'timeout' failed to satisfy constraint: Member must have value less than or equal to 900 (Service: AWSLambdaInternal; Status Code: 400; Error Code: ValidationException; Request ID: b2debad7-3ee6-432f-a3ce-c18592332371).
-    logger.info("Received event is {}".format(event)) # {key1: value1, key2: value2 } is passed in as event 
+    logger.info("Received event is {}".format(event)) # {key1: value1, key2: value2 } is passed in as event
+    logger.info("Service wide environment variable is {}".format(os.getenv('service_wide_env_variable')))
     logger.info("Received context is {}".format(context))
     for i in range(1200):
         logger.info("Iteration {}".format(i))
