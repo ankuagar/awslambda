c1b4f2a9134cb1782d55ff2fc3af84a70c82d530
Ankur Agarwal <ankur.a.agarwal@gmail.com>
2019-09-28 23:59:35 -0700

    Add function specific environment variable

--- src/handler.py
+++ src/handler.py
@@ -17,7 +17,8 @@ def lambda1(event, context):
         "statusCode": 200,
         "body": json.dumps(body)
     }
-    logger.info("Service wide environment variable is {}".format(os.getenv('service_wide_env_variable')))
+    logger.info("Service wide environment variable value is {}".format(os.getenv('service_wide_env_variable')))
+    logger.info("Function specific environment variable value is {}".format(os.getenv('my_simple_lambda_function_env_variable')))
     logger.info("Response is {}".format(response))
     return response
 
@@ -36,6 +37,7 @@ def lambda2(event, context):
     #An error occurred: MyDashsimpleDashlambdaDashfunctionLambdaFunction - 1 validation error detected: Value '1200' at 'timeout' failed to satisfy constraint: Member must have value less than or equal to 900 (Service: AWSLambdaInternal; Status Code: 400; Error Code: ValidationException; Request ID: b2debad7-3ee6-432f-a3ce-c18592332371).
     logger.info("Received event is {}".format(event)) # {key1: value1, key2: value2 } is passed in as event
     logger.info("Service wide environment variable is {}".format(os.getenv('service_wide_env_variable')))
+    logger.info("Function specific environment variable value is {}".format(os.getenv('lambda_that_simply_sleeps_env_variable')))
     logger.info("Received context is {}".format(context))
     for i in range(1200):
         logger.info("Iteration {}".format(i))
